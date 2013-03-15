a963d92cdc31f76acc15fa77fc57cdb6a7d21c6c
Anton <akannwetr@gmail.com>
2013-03-15 21:52:28 +0300

    Remade functions. Simplier(slower) methods.
    No character case check. No sorting on other than surname(last name).
    Slower sorting. Due to technical reasons indent isn't fixed, sorry.

--- main.c
+++ main.c
@@ -1,148 +1,132 @@
 #include "stdio.h"
 #include "stdlib.h"
+#ifndef _WIN32 || ifndef _WIN64
+	#include "std_ext.h"
+    #define  fflush __fpurge
+#endif
 
-typedef struct company {
-    char names[3][30];
-    int salary[12];
-    int sort_priority[3];
-    char *adv_sorting[3];
-} com_t;
-
-typedef struct sorting {
-    int sort_priority[3];
-    int sort_order;
-} sor_t;
-
-int input(com_t *);
-int sort_up(com_t *, int);
-int matrix_reloaded(com_t *, char **, int);
-
-int main(int argc, char **argv)
+typedef struct employees
 {
-    com_t *company;
-    int workers_n, i;
-    char **table;
-    company = (com_t *) calloc(25, sizeof(com_t));
-    workers_n = (input(company));
-    table = (char **) calloc(workers_n + 1, sizeof(char *));
-    for (i = 0; i < workers_n + 1; i++)
-        *(table + i) = (char *) malloc(sizeof(int) * 12 + 45);
-    sort_up(company, workers_n);
-    matrix_reloaded(company, table, workers_n);
-    for (i = 0; i < workers_n + 1; i++)
-        printf("%s\n", table[i]);
-    return (0);                 /* I like it */
+	char names[3][30];
+	int salary[12];
+} employ_t;
+
+int input(employ_t*);
+int sort_up(employ_t*,int);
+int get_num(int*);
+int output_table(employ_t*);
+char* month_name(int);
+
+int main(int argc,char **argv)
+{
+	employ_t *company;
+	int workers_n,i,j;
+	char name[35];
+	company=(employ_t*)calloc(25,sizeof(employ_t));
+	workers_n=(input (company));
+	company=(employ_t*)realloc(company,workers_n*sizeof(employ_t));
+	sort_up(company,workers_n);
+	output_table(company);
+	free(company);
+	return (0);                    /* I like it */
 }
 
-int input(com_t * company)
+int input(employ_t* company)
 {
-    char *month[12] =
-        { "January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December" };
-    int i = 0, j = 0;
-    for (i = 0; i < 25; i++) {
-        printf("Provide First Name. To end input press enter.\n");
-        fgets(company[i].names[0], 30, stdin);
-        if (*company[i].names[0] == '\n')
-            break;
-        while (*(company[i].names[0] + j) != '\n')
-            j++;
-        *(company[i].names[0] + j) = '\0';
-        printf("Last Name\n");
-        fgets(company[i].names[1], 30, stdin);
-        while (*(company[i].names[0] + j) != '\n')
-            j++;
-        *(company[i].names[0] + j) = '\0';
-        printf("Patronymic\n");
-        fgets(company[i].names[2], 30, stdin);
-        while (*(company[i].names[0] + j) != '\n')
-            j++;
-        *(company[i].names[0] + j) = '\0';
-        for (j = 0; j < 12; j++) {
-            printf("Provide salary for %s\n", month[j]);
-            scanf("%d", &company[i].salary[j]);
-        }
-        for (j = 0; j < 3; j++) {
-            company[i].adv_sorting[j] = &company[i].names[j][1];
-            if (*(company[i].names[j]) > 'a' - 1)
-                company[i].sort_priority[j] =
-                    *(company[i].names[j]) - 'a' + 'A';
-            else
-                company[i].sort_priority[j] = *(company[i].names[j]);
-        }
-    }
-    return i;
+	int i=0,j=0,k=0;
+	for (i=0;i<25;i++)
+	{
+		printf ("Provide First Name. To end input press enter.\n");
+		fflush(stdin);
+		fgets (company[i].names[0],29,stdin);
+		if (*company[i].names[0]=='\n')
+			break;
+		printf ("Last Name\n");
+		fflush(stdin);
+		fgets (company[i].names[1],29,stdin);
+		printf ("Patronymic\n");
+		fflush(stdin);
+		fgets (company[i].names[2],29,stdin);
+		for (j=0;j<12;j++)
+		{
+			printf ("Provide salary for %s\n",month_name(j));
+			fflush(stdin);
+			while (!get_num(&company[i].salary[j]))
+				printf ("Please print numbers. NUMBERS.\n");
+		}
+		for (j=0;j<3;j++)
+		{
+			while (*(company[i].names[j]+k)!='\n')
+			k++;
+			*(company[i].names[j]+k)='\0';
+			k=0;
+		}
+	}
+	return i;
 }
 
-int sort_up(com_t * company, const int workers_n)
+int get_num(int* target)
 {
-    int i = 0, j = 0, k = 0, min, s_type;
-    sor_t *sort;
-    sor_t tmp;
-    com_t *temp;
-    sort = (sor_t *) calloc(workers_n, sizeof(sor_t));
-    temp = (com_t *) calloc(workers_n, sizeof(com_t));
-    printf
-        ("You want to sort by:\n1)First Name\n2)Last Name\n3)Patronymic\n");
-    scanf("%d", &s_type);
-    s_type--;
-    for (i = 0; i < workers_n; i++)
-        for (j = 0; j < 3; j++) {
-            sort[i].sort_priority[j] = company[i].sort_priority[j];
-            sort[i].sort_order = i;
-        }
-    for (i = 0; i < workers_n; i++) {
-        min = i;
-        for (j = i + 1; j < workers_n; j++)
-            if (sort[j].sort_priority[s_type] <=
-                sort[min].sort_priority[s_type])
-                if (sort[j].sort_priority[s_type] ==
-                    sort[min].sort_priority[s_type]) {
-                    while (*
-                           (company[sort[j].sort_order].
-                            adv_sorting[s_type] + k) ==
-                           *(company[sort[min].sort_order].
-                             adv_sorting[s_type] + k))
-                        k++;
-                    if (*
-                        (company[sort[j].sort_order].adv_sorting[s_type] +
-                         k) <
-                        *(company[sort[min].sort_order].
-                          adv_sorting[s_type] + k))
-                        min = j;
-                } else
-                    min = j;
-        tmp = sort[min];
-        sort[min] = sort[i];
-        sort[i] = tmp;
-    }
-    for (i = 0; i < workers_n; i++)
-        temp[i] = company[sort[i].sort_order];
-    for (i = 0; i < workers_n; i++)
-        company[i] = temp[i];
-    free(sort);
-    free(temp);
-    return 1;
+	char tmp='a';
+	char buff[10];
+	int i=0,output=0;
+	fgets (buff,8,stdin);
+	if ((tmp=buff[i])=='\n')
+		return 0;
+	do
+	{
+		if (tmp>'9')
+			return 0;
+		output=output*10+(tmp-'0');
+		i++;
+		tmp=buff[i];
+	} while (tmp && tmp!='\n');
+	*target=output;
+	return 1;
 }
 
-int matrix_reloaded(com_t * company, char **table, int workers_n)
+char* month_name(int i)
 {
-    int i = 0;
-    char name[30] = { "Name" };
-    sprintf(*table,
-            "%-20s  Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec",
-            name);
-    for (i = 0; i < workers_n; i++) {
-        sprintf(name, "%s %c. %c.", company[i].names[1],
-                *(company[i].names[0]), *(company[i].names[2]));
-        sprintf(*(table + i + 1),
-                "%-20s%5d %5d %5d %5d %5d %5d %5d %5d %5d %5d %5d %5d\n",
-                name, company[i].salary[0], company[i].salary[1],
-                company[i].salary[2], company[i].salary[3],
-                company[i].salary[4], company[i].salary[5],
-                company[i].salary[6], company[i].salary[7],
-                company[i].salary[8], company[i].salary[9],
-                company[i].salary[10], company[i].salary[11]);
-    }
-    return 1;
+	char* month[]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+	return month[i];
+}
+
+int sort_up(employ_t* company,int workers_n)
+{
+	int i=0,j=0,k=0,min;
+	employ_t temp;
+	for (i=0;i<workers_n;i++)
+	{
+		min=i;
+		for (j=i+1;j<workers_n;j++)
+			if (*(company[j].names[1])<*(company[min].names[1]))
+				min=j;
+			else if (*(company[j].names[1])==*(company[i].names[1]))
+			{
+				while (*(company[j].names[1]+k)==*(company[i].names[1]+k))
+					k++;
+				if (*(company[j].names[1]+k)<*(company[i].names[1]+k))
+					min=j;
+			}
+		temp=company[min];
+		company[min]=company[i];
+		company[i]=temp;
+	}
+	return 1;
+}
+
+int output_table(employ_t* comapany)
+{
+	printf ("Name%16c",' ');
+	for (i=0;i<12;i++)
+		printf ("%9s",month_name(i));
+	printf ("\n");
+	for (i=0;i<workers_n;i++)
+	{
+		sprintf (name,"%s %c. %c.",company[i].names[1],*(company[i].names[0]),*(company[i].names[2]));
+		printf ("%-20s",name);
+		for (j=0;j<12;j++)
+			printf ("%9d",company[i].salary[j]);
+	}
+	return 1;
 }
